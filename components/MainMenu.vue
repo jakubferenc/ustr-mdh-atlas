7a380235268180c96395136e0c103d75b4e58cc0
Jakub Albert Ferenc <jakub.ferenc@spaceflow.io>
2023-01-01 05:42:39 +0100

    foix active link for detail page

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -2,11 +2,12 @@
 nav.main-nav(role="navigation" aria-label="Primary")
   NuxtLink(to="/").item Hlavní stránka
   NuxtLink(to="/prochazky/").item.item--prochazky Procházky
-  NuxtLink(to="/me-objekty/" ).item Mé objekty
+  NuxtLink(to="/me-objekty/" ).item.item--objekty Mé objekty
   NuxtLink(to="/o-projektu/" ).item O projektu
 </template>
 
 <style lang="sass" scoped>
+html.page--objekty .item--objekty,
 html.page--prochazka .item--prochazky
   text-decoration: underline
 </style>
