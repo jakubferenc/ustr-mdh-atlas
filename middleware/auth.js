6b8a3afa5953a9381ef5dba33432265bb3488157
Jakub Albert Ferenc <jakub.ferenc@spaceflow.io>
2023-01-02 07:15:34 +0100

    allow additional pages without login

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,10 +3,17 @@ export default async function ({ redirect, store, route, $fire, router }) {
 
   const isRestrictedPage =
     !isUserSignedIn &&
-    !['/prihlaseni/', '/registrace/', '/sdilet/', '/', '/prochazky/'].includes(
-      route.path
-    ) &&
-    !['/sdilet-objekt/:id?/'].includes(route.matched[0].path);
+    ![
+      '/prihlaseni/',
+      '/registrace/',
+      '/sdilet/',
+      '/',
+      '/prochazky/',
+      '/o-projektu/',
+    ].includes(route.path) &&
+    !['/prochazka/:slug?/', '/sdilet-objekt/:id?/'].includes(
+      route.matched[0].path
+    );
   if (isRestrictedPage) {
     redirect('/prihlaseni/');
     console.log('user is not logged in');
